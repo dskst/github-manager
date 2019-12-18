3f7efbcb062c3a89cd9ac5fd4641fab02f3094ff
Daisuke Sato <grandevivo9-dskst@yahoo.co.jp>
2019-12-18 17:55:13 +0900

    Use ncc build

---
+++ runs/dist/index.js
@@ -0,0 +1,76 @@
+module.exports =
+/******/ (function(modules, runtime) { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	__webpack_require__.ab = __dirname + "/";
+/******/
+/******/ 	// the startup function
+/******/ 	function startup() {
+/******/ 		// Load entry module and return exports
+/******/ 		return __webpack_require__(363);
+/******/ 	};
+/******/
+/******/ 	// run startup
+/******/ 	return startup();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 352:
+/***/ (function() {
+
+eval("require")("@actions/core");
+
+
+/***/ }),
+
+/***/ 363:
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+
+const core = __webpack_require__(352);
+const github = __webpack_require__(932);
+
+try {
+    console.log('Hello');
+} catch (error) {
+    core.setFailed(error.message);
+}
+
+/***/ }),
+
+/***/ 932:
+/***/ (function() {
+
+eval("require")("@actions/github");
+
+
+/***/ })
+
+/******/ });
