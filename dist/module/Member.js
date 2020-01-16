a780886e5df586237b8063ede5b12246e27a2c9c
Daisuke Sato <grandevivo9-dskst@yahoo.co.jp>
2020-01-16 10:28:24 +0900

    WIP DI

---
+++ dist/module/Member.js
@@ -0,0 +1,9 @@
+"use strict";
+class Member {
+    constructor() {
+    }
+    isExists() {
+        return true;
+    }
+}
+module.exports = Member;
--- src/module/Member.ts
+++ src/module/Member.ts
@@ -1,10 +1,22 @@
+import * as github from '@actions/github';
+const HttpsProxyAgent = require("https-proxy-agent");
+
 class Member {
 
     constructor() {
     }
 
-    public isExists(): boolean {
-        return true;
+    public isExists(): Promise<boolean> {
+        const gitMember = new github.GitHub(
+            ''
+            //,{request: {agent: new HttpsProxyAgent(process.env.https_proxy)}}
+        );
+        const response = gitMember.users.getByUsername({username:'dskst'});
+        return response.then((value) => {
+            return value.data.login != null;
+        }).catch((error) => {
+            return false;
+        });
     }
 }
 
