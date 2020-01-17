39a6a806c9f7d21257fd5ab503829f9cadb9d508
Daisuke Sato <grandevivo9-dskst@yahoo.co.jp>
2020-01-18 01:13:56 +0900

    Fix typo

--- tests/usecase/Member.test.ts
+++ tests/usecase/Member.test.ts
@@ -2,8 +2,8 @@ import Member from '../../src/usecase/Member';
 import MemberRepositoryImpl from '../../src/infrastructure/repository/MemberRepositoryImpl'
 
 describe('Member', () => {
-     describe('isExists', () => {
-          test('Is exist on GitHub', () => {
+     describe('exists', () => {
+          test('Exists on GitHub', () => {
                // TODO: Change octkit to nock
                return expect(new Member(new MemberRepositoryImpl()).exists()).resolves.toBeTruthy()
           })
